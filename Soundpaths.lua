94505ed6899c04d45f9bd645c84325f30e3c4e6d
Alec <Alec.Gamble@live.co.uk>
2024-10-22 16:54:50 +0100

    1.0 - Created addon, added some SFX

---
+++ Soundpaths.lua
@@ -0,0 +1,41 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+local Name = "|cffeb008b[|r|cfff55b9fJ|r|cfffc89b5J|r|cffffb2ccB|r|cffffd9e5A|r|cffffffff]|r "
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+
+-- -----
+--   SOUND
+-- -----
+
+LSM:Register("sound", Name.."Freeze", [[Interface\Addons\JJBA_Soundpack\sound\Freeze.ogg]])
+LSM:Register("sound", Name.."Giorno", [[Interface\Addons\JJBA_Soundpack\sound\Giorno.ogg]])
+LSM:Register("sound", Name.."Goodbye JoJo", [[Interface\Addons\JJBA_Soundpack\sound\GoodbyeJoJo.ogg]])
+LSM:Register("sound", Name.."Jaggers", [[Interface\Addons\JJBA_Soundpack\sound\Jaggers.ogg]])
+LSM:Register("sound", Name.."Kono Dio Da", [[Interface\Addons\JJBA_Soundpack\sound\KonoDioDa.ogg]])
+LSM:Register("sound", Name.."Lelolelolelo", [[Interface\Addons\JJBA_Soundpack\sound\Lelolelolelolelo.ogg]])
+LSM:Register("sound", Name.."Muda Muda", [[Interface\Addons\JJBA_Soundpack\sound\MudaMuda.ogg]])
+LSM:Register("sound", Name.."Nice", [[Interface\Addons\JJBA_Soundpack\sound\Nice.ogg]])
+LSM:Register("sound", Name.."No No No No No", [[Interface\Addons\JJBA_Soundpack\sound\NoNoNoNoNo.ogg]])
+LSM:Register("sound", Name.."Oh No 1", [[Interface\Addons\JJBA_Soundpack\sound\OhNo_01.ogg]])
+LSM:Register("sound", Name.."Oh No 2", [[Interface\Addons\JJBA_Soundpack\sound\OhNo_02.ogg]])
+LSM:Register("sound", Name.."Oh That's A Baseball!", [[Interface\Addons\JJBA_Soundpack\sound\OhThatsABaseball.ogg]])
+LSM:Register("sound", Name.."Ora Ora", [[Interface\Addons\JJBA_Soundpack\sound\OraOra.ogg]])
+LSM:Register("sound", Name.."The Hand", [[Interface\Addons\JJBA_Soundpack\sound\TheHand.ogg]])
+LSM:Register("sound", Name.."Unfreeze", [[Interface\Addons\JJBA_Soundpack\sound\Unfreeze.ogg]])
+LSM:Register("sound", Name.."Yes Yes Yes Yes Yes", [[Interface\Addons\JJBA_Soundpack\sound\YesYesYesYes.ogg]])
+LSM:Register("sound", Name.."Za Warudo", [[Interface\Addons\JJBA_Soundpack\sound\ZaWarudo.ogg]])
+
+-- -----
+--   STATUSBAR
+-- -----
