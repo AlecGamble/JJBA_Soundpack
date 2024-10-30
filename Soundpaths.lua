f90f99df56a2ee1a517d5c2ae3cd36038c2043ec
AlecGamble <Alec.Gamble@live.co.uk>
2024-10-30 19:44:11 +0000

    Added More Sfx
    - Removed duplicate sound "Unfreeze"
    - Added Sound "Caesar"
    - Added Sound "Hey Baby"
    - Added Sound "Kira's Theme"
    - Added Sound "Sonna Da Beetch!"
    - Added Sound "To Be Continued 1"
    - Added Sound "To Be Continued 2"

--- Soundpaths.lua
+++ Soundpaths.lua
@@ -17,11 +17,13 @@ local Name = "|cffeb008b[|r|cfff55b9fJ|r|cfffc89b5J|r|cffffb2ccB|r|cffffd9e5A|r|
 -- -----
 --   SOUND
 -- -----
-
+LSM:Register("sound", Name.."Caesar!!!", [[Interface\Addons\JJBA_Soundpack\sound\Caesar.ogg]])
 LSM:Register("sound", Name.."Freeze", [[Interface\Addons\JJBA_Soundpack\sound\Freeze.ogg]])
 LSM:Register("sound", Name.."Giorno", [[Interface\Addons\JJBA_Soundpack\sound\Giorno.ogg]])
 LSM:Register("sound", Name.."Goodbye JoJo", [[Interface\Addons\JJBA_Soundpack\sound\GoodbyeJoJo.ogg]])
+LSM:Register("sound", Name.."Hey Baby!", [[Interface\Addons\JJBA_Soundpack\sound\HeyBaby.ogg]])
 LSM:Register("sound", Name.."Jaggers", [[Interface\Addons\JJBA_Soundpack\sound\Jaggers.ogg]])
+LSM:Register("sound", Name.."Kira's Theme", [[Interface\Addons\JJBA_Soundpack\sound\KiraTheme.ogg]])
 LSM:Register("sound", Name.."Kono Dio Da", [[Interface\Addons\JJBA_Soundpack\sound\KonoDioDa.ogg]])
 LSM:Register("sound", Name.."Lelolelolelo", [[Interface\Addons\JJBA_Soundpack\sound\Lelolelolelolelo.ogg]])
 LSM:Register("sound", Name.."Muda Muda", [[Interface\Addons\JJBA_Soundpack\sound\MudaMuda.ogg]])
@@ -31,8 +33,11 @@ LSM:Register("sound", Name.."Oh No 1", [[Interface\Addons\JJBA_Soundpack\sound\O
 LSM:Register("sound", Name.."Oh No 2", [[Interface\Addons\JJBA_Soundpack\sound\OhNo_02.ogg]])
 LSM:Register("sound", Name.."Oh That's A Baseball!", [[Interface\Addons\JJBA_Soundpack\sound\OhThatsABaseball.ogg]])
 LSM:Register("sound", Name.."Ora Ora", [[Interface\Addons\JJBA_Soundpack\sound\OraOra.ogg]])
+LSM:Register("sound", Name.."Sonna Da Beetch", [[Interface\Addons\JJBA_Soundpack\sound\SonnaDaBeetch.ogg]])
 LSM:Register("sound", Name.."The Hand", [[Interface\Addons\JJBA_Soundpack\sound\TheHand.ogg]])
-LSM:Register("sound", Name.."Unfreeze", [[Interface\Addons\JJBA_Soundpack\sound\Unfreeze.ogg]])
+LSM:Register("sound", Name.."To Be Continued 1", [[Interface\Addons\JJBA_Soundpack\sound\ToBeContinued_01.ogg]])
+LSM:Register("sound", Name.."To Be Continued 2", [[Interface\Addons\JJBA_Soundpack\sound\ToBeContinued_02.ogg]])
+LSM:Register("sound", Name.."The Hand", [[Interface\Addons\JJBA_Soundpack\sound\TheHand.ogg]])
 LSM:Register("sound", Name.."Yes Yes Yes Yes Yes", [[Interface\Addons\JJBA_Soundpack\sound\YesYesYesYes.ogg]])
 LSM:Register("sound", Name.."Za Warudo", [[Interface\Addons\JJBA_Soundpack\sound\ZaWarudo.ogg]])
 
